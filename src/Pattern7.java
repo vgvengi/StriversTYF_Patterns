e3b5ca138110f28f9d98c966aaf6fcf82d27a073
vgvengi <vengateshvg1123@gmail.com>
2025-07-07 22:41:07 +0530

    first commit

--- src/Pattern7.java
+++ src/Pattern7.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Pattern7 {
 
     public static void pattern7(int n){
@@ -20,6 +22,7 @@ public class Pattern7 {
 
     public static void main(String args[]){
         pattern7(5);
+
     }
 }
 
---
+++ src/TrianglePattern.java
@@ -0,0 +1,26 @@
+public class TrianglePattern {
+    public static void pattern(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            //spacing
+            for(int j=1;j<=n-i;j++) {
+                System.out.print(" ");
+            }
+
+            //star
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+            }
+            //spacing
+            for (int j = 1; j<= n-i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        pattern(5);
+    }
+}
+
