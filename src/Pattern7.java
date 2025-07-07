3ce06dccbfe527dfaccd1d49d1f4b927232e0454
vgvengi <vengateshvg1123@gmail.com>
2025-07-07 20:35:13 +0530

    first commit

--- src/Pattern7.java
+++ src/Pattern7.java
@@ -3,15 +3,15 @@ public class Pattern7 {
     public static void pattern7(int n){
         for(int i=1;i<=n;i++){
             //spacing
-            for(int j=1;j<=n-i;j++){
+            for(int j=1;j<i;j++){
                 System.out.print(" ");
             }
             //star
-            for(int j=1;j<=2*i-1;j++){
+            for(int j=1;j<=n*2-i-i+1;j++){
                 System.out.print("*");
             }
             //spacing
-            for(int j=1;j<=n-i;j++){
+            for(int j=1;j<i;j++){
                 System.out.print(" ");
             }
             System.out.println( );
